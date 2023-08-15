58910674c75c6c7fb8077a42110ed0c31f645b43
Nilesh4321 <nileshpushkarna1234@gmail.com>
2023-08-15 14:07:43 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import NewsApp from './News';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <nav class="navbar navbar-expand-lg  bg-dark border-bottom border-body ">
+        <div class="container-fluid bg-dark  d-flex justify-content-center">
+          <a class="navbar-brand text-danger" href="#">OmEgA <span class="navbar-brand text-light">nEws</span></a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </nav>
+      <NewsApp />
+    </>
   );
 }
 
